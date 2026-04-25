31dab1203b7feeca688fd03cbbfeff08fe785dae
Shady <shady.mohamed.mech@gmail.com>
2026-04-25 20:23:17 +0300

    Removed TODO comments

--- main.cpp
+++ main.cpp
@@ -377,10 +377,6 @@ Image rotate90(const Image &input)
     int channels = input.getChannels();
     Image output(height, width, channels); // Width and height are swapped
 
-    //   TODO: Implement this function
-    //   For each pixel and each channel:
-    //   output(x, height-1-y, c) = input(y, x, c)
-
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             for (int c = 0; c < channels; c++) {
